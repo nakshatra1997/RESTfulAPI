eb182a86986091222cbddd04c4dd1367638e20fc
nakshatra1997 <nakshatrapradhan2013@gmail.com>
2017-08-26 13:32:04 +0100

    added my_ide_helper.php as methods like nullable,default,unique were not there in fluent.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -16,9 +17,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+//          $table->string('email')->unique();   unique method not found
+            $table->string('email');
+            $table->unique('email');
             $table->string('password');
             $table->rememberToken();
+            $table->string('verified')->default(User::UNVERIFIED_USER);
+            $table->string('verification_token')->nullable();
+            $table->string('admin')->default(User::REGULAR_USER);
             $table->timestamps();
         });
     }
