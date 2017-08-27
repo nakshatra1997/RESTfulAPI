b42c84ae3bc8fc42dd59322efd4edd46b9c8f049
nakshatra1997 <nakshatrapradhan2013@gmail.com>
2017-08-27 23:40:32 +0100

    working on index of buyer but the response is not visible although the status is ok

--- app/Http/Controllers/Buyer/BuyerController.php
+++ app/Http/Controllers/Buyer/BuyerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Buyer;
 
+use App\Buyer;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -15,6 +16,9 @@ class BuyerController extends Controller
     public function index()
     {
         //
+        $buyers=Buyer::has('transactions')->get();
+        return response()->json(['data'=>$buyers],200);
+        //return response not visible
     }
 
     /**
