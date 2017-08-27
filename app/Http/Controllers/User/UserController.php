911b4c3234d6ede932f6e1cfd2ca06a9290ce3da
nakshatra1997 <nakshatrapradhan2013@gmail.com>
2017-08-27 23:23:21 +0100

    user functionality is done but delete functionality encountered error asSQLSTATE[23000]: Integrity constraint violation: 1451 Cannot delete or update a parent row: a foreign key constraint fails (., CONSTRAINT  FOREIGN KEY () REFERENCES  (uid=197608(pradhan) gid=197121(None) groups=197121(None),545(Users),4(INTERACTIVE),66049(CONSOLE LOGON),11(Authenticated Users),15(This Organization),113(Local account),4095(CurrentSession),66048(LOCAL),262154(NTLM Authentication),401408(Medium Mandatory Level))) (SQL: delete from  where uid=197608(pradhan) gid=197121(None) groups=197121(None),545(Users),4(INTERACTIVE),66049(CONSOLE LOGON),11(Authenticated Users),15(This Organization),113(Local account),4095(CurrentSession),66048(LOCAL),262154(NTLM Authentication),401408(Medium Mandatory Level) = 101)

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -31,6 +31,19 @@ class UserController extends Controller
     public function store(Request $request)
     {
         //
+        $rules=[
+            'name'=>'required',
+            'email'=>'required|email|unique:users',
+            'password'=>'required|min:6|confirmed',
+        ];
+        $this->validate($request,$rules);
+        $data=$request->all();
+        $data['password']=bcrypt($request->password);
+        $data['verified']=User::UNVERIFIED_USER;
+        $data['verification_token']=User::generateVerificationCode();
+        $data['admin']=User::REGULAR_USER;
+        $user=User::create($data);
+        return response()->json(['data'=>$user],201);
     }
 
     /**
@@ -42,6 +55,8 @@ class UserController extends Controller
     public function show($id)
     {
         //
+        $user=User::findOrFail($id);
+        return response()->json(['data'=>$user],200);
     }
 
 
@@ -56,6 +71,41 @@ class UserController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $user=User::findOrFail($id);
+        $rules=[
+            'email'=>'email|unique:users,email,'.$user->id,
+            'password'=>'min:6|confirmed',
+            'admin'=>'in:'.User::ADMIN_USER.','.User::REGULAR_USER,
+        ];
+        if($request->has('name'))
+        {
+            $user->name=$request->name;
+        }
+        if($request->has('email')&&$user->email!=$request->email)
+        {
+            $user->verified=User::UNVERIFIED_USER;
+            $user->verification_token=User::generateVerificationCode();
+            $user->email=$request->email;
+        }
+        if($request->has('password'))
+        {
+            $user->password=bcrypt($request->password);
+        }
+        if($request->has('admin'))
+        {
+            if(!$user->isVerified())
+            {
+                 return response()->json(['error'=>'only verified users can modify the admin field','code'=>409],409);
+            }
+            $user->admin=$request->admin;
+        }
+        if(!$user->isDirty())
+        {
+           return response()->json(['error'=>'u need to specify a different value','code'=>422],422);
+        }
+        $user->save();
+        return response()->json(['data'=>$user],200);
+
     }
 
     /**
@@ -67,5 +117,8 @@ class UserController extends Controller
     public function destroy($id)
     {
         //
+        $user=User::findOrFail($id);
+        $user->delete();
+        return response()->json(['data'=>$user],200);
     }
 }
